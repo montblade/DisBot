0a5ec2b8231c671fe34b8cc282135594642841ee
Kuba <yonikusiek@gmail.com>
2020-04-15 00:32:06 +0200

    Add new files

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'Njk2NDgwNTY5NTUwMTc2MzY4.XopWNg.-TRbcvgrTMiGZOndsJRzH6_KvXU';
+
+bot.on('ready', () =>{
+    console.log('This bot is online!');
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "HELLO"){
+        msg.reply('HELLO FRIEND');
+    }
+})
+
+
+bot.login(token);
